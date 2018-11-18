766f4cbb76b1ca7ce3e8acd4e5a67455f4057b77
krepe-suZette <37508580+krepe-suZette@users.noreply.github.com>
2018-11-18 19:57:15 +0900

    Update in100_input.py - change words to English

--- code/in100_input.py
+++ code/in100_input.py
@@ -14,10 +14,10 @@ sql = (
 )
 
 event_name = config['event_name']
-today = input('오늘 날짜 입력: ')
+today = input('Input date: ')
 
 rank2input = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-args = [(today, f"inRanking{n:0>3}", event_name, input(f'{n:>3}등 입력: '), 0, n, "수동 입력", 0) for n in rank2input]
+args = [(today, f"inRanking{n:0>3}", event_name, input(f'Input Rank {n:>3}: '), 0, n, "수동 입력", 0) for n in rank2input]
 
 cur.executemany(sql, args)
 conn.commit()
